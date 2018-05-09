91f0821ac7e616bbb33a98ef19e1aecc456264d3
asheshvasalya_at_yahoo_in <asheshvasalya_at_yahoo_in@gite.lirmm.fr>
2018-05-09 17:49:43 +0900

    build dir removed

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/avasalya/mc_handover_controller
-# Build directory: /home/avasalya/mc_handover_controller/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/avasalya/mc_handover_controller/src
-# Build directory: /home/avasalya/mc_handover_controller/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
